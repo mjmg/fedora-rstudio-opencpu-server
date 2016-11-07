cb88253ea87552355ca38671931429a9d5a6f9c3
mjmg <mjmg@users.noreply.github.com>
2016-11-07 19:05:24 +0800

    Update installpackages.sh

--- installpackages.sh
+++ installpackages.sh
@@ -9,9 +9,12 @@ echo "Installing dplyr and hadley from github"
 Rscript -e "library(devtools); install_github('dplyr', 'hadley')"
 
 # This installs opencpu webapps from github
-echo "Installing appdemo and gitstats opencpu webapp"
+echo "Installing appdemo, gitstats, tvscore and qitools/charts opencpu webapp"
 Rscript -e "library(devtools); install_github('mjmg/appdemo')"
 Rscript -e "library(devtools); install_github('mjmg/gitstats')"
+Rscript -e "library(devtools); install_github('mjmg/tvscore')"
+Rscript -e "library(devtools); install_github('qitools/charts')"
+
 
 # This installs R packages under Bioconductor
 echo "Installing EBImage from Bioconductor"
